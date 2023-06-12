8263b06c102e1341a505fd8ad0fc4260f402091f
ank809 <ankita51523@gmail.com>
2023-06-13 00:08:03 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main(){
+  runApp(Roll_Dice());
+}
+
+class Roll_Dice extends StatefulWidget {
+  const Roll_Dice({super.key});
+
+  @override
+  State<Roll_Dice> createState() => _Roll_DiceState();
+}
+
+class _Roll_DiceState extends State<Roll_Dice> {
+  var dice_numbber=2;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        appBar: AppBar(title: Text('Roll the dice ' ,) , backgroundColor: Color.fromARGB(255, 98, 59, 202), centerTitle: true,),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.only( top:40.0),
+            child: Expanded(
+              child: Column(
+                children:<Widget> [
+                  Text('Roll the dice given below ' , style: TextStyle(fontSize: 30.0),),
+                  SizedBox(height: 65.0),
+                  TextButton(
+                    onPressed: (){
+                    setState(() {
+                      // for 0-5 as we do not have any image with 0 so we add 1 in it 
+                      dice_numbber= Random().nextInt(6)+1;
+                    });
+                  }, child:  Container(
+                    width: 300.0,
+                    child: BounceInUp(child: Image.asset('Assets/Images/dice$dice_numbber.png')),
+                    ),
+                    ),
+                    SizedBox(height: 45.0,),
+                    BounceInUp(child: Text('The dice number is $dice_numbber',  style: TextStyle(fontSize: 32.0),)),
+                ],
+              ),
+            ),
+          ), ),
+        ),
+    );
+  }
+}
